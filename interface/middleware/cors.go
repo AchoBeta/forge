16de858b48deea6e69c3821fe63a8fb3bef82c1b
zhengjun0120 <zhengjun0120@163.com>
2025-11-15 19:38:19 +0800

    fix:修复小bug

--- interface/middleware/cors.go
+++ interface/middleware/cors.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"forge/infra/configs"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -8,7 +9,7 @@ import (
 func CorsMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
 
-		AllowOrigins:  []string{"*"},
+		AllowOrigins:  []string{configs.Config().GetAppConfig().YourFrontendDomain},
 		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		AllowHeaders:  []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders: []string{"Content-Length", "Authorization"},
